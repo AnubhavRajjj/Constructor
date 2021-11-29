9b3cabe37d47158d531165003ecbd63b1ea9d11c
Anubhav Raj <anubhavraj21082002@gmail.com>
2021-11-29 20:28:51 +0530

    question based on Volume

---
+++ src/Box.java
@@ -0,0 +1,22 @@
+public class Box {
+    double width;
+    double height;
+    double depth;
+    double total;
+
+    Box(double width, double height,double depth ){
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+    public double volume(){
+        total=(width*height*depth);
+        return total;
+
+    }
+
+    public static void main(String[] args) {
+        Box obj = new Box(4,2,0.5);
+        System.out.println(obj.volume());
+    }
+}
